fe72650af6d4caac6187c45d5070699e68b6b8b4
John Cleaver <cleaver.john.k@gmail.com>
2018-02-25 10:07:02 -0500

    Add footer to app.

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <div id="app">
     <header-bar/>
     <router-view/>
+    <app-footer/>
   </div>
 </template>
 
 <script>
 import HeaderBar from '@/components/HeaderBar'
+import AppFooter from '@/components/AppFooter'
+
 export default {
   name: 'App',
   components: {
-    HeaderBar
+    HeaderBar,
+    AppFooter
   }
 }
 </script>
