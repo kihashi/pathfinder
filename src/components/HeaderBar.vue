99d823039c0e3ebad8a79674936bec3df75c8511
John Cleaver <cleaver.john.k@gmail.com>
2018-02-25 10:06:22 -0500

    Add automated routing nav to header.

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,23 +1,27 @@
 <template>
-  <nav class="navbar">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="/">
-        <h1 class="title">Pathfinder</h1>
-      </a>
-      <div class="navbar-burger"
-           v-bind:class="{ 'is-active': menu_active }"
-           @click="menu_active = !menu_active"
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-    <div class="navbar-menu"
-      v-bind:class="{ 'is-active': menu_active }">
-      <router-link v-for="route in this.$router.options.routes" :key="route.order" class="navbar-item" :to="route">{{route.name}}</router-link>
-    </div>
-  </nav>
+  <section class="hero is-info is-bold">
+      <nav class="navbar">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="/">
+            <h1 class="title">Pathfinder</h1>
+          </a>
+          <div class="navbar-burger"
+               v-bind:class="{ 'is-active': menu_active }"
+               @click="menu_active = !menu_active"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div class="navbar-menu"
+          v-bind:class="{ 'is-active': menu_active }">
+          <div class="navbar-end">
+            <router-link v-for="route in this.$router.options.routes" :key="route.order" class="navbar-item" :to="route">{{route.name}}</router-link>
+          </div>
+        </div>
+      </nav>
+  </section>
 </template>
 
 <script>
